b9bf1122bc6498d86f398a7366d2b13da130b0af
telio <telioletalle@gmail.com>
2025-05-20 11:58:25 +0200

    Fix typo in page title for agenda module

--- agenda/agenda.php
+++ agenda/agenda.php
@@ -408,7 +408,7 @@ function generateMiniCalendar($month, $year, $selected_date = null) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Agenda Pronote</title>
+  <title>Agaenda Pronote</title>
   <link rel="stylesheet" href="assets/css/calendar.css">
   <style>
     /* Structure principale */
